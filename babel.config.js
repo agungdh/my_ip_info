a982a8b601dc2841414dbe6d1d2a3e199545aac0
AgungDH <agungdh@live.com>
2021-06-25 14:29:57 +0700

    add .env

--- babel.config.js
+++ babel.config.js
@@ -4,8 +4,18 @@ module.exports = function(api) {
     presets: ['babel-preset-expo'],
     env: {
       production: {
-        plugins: ['react-native-paper/babel'],
+        plugins: ['react-native-paper/babel']
       },
     },
+    "plugins": [
+      ["module:react-native-dotenv", {
+        "moduleName": "@env",
+        "path": ".env",
+        "blacklist": null,
+        "whitelist": null,
+        "safe": false,
+        "allowUndefined": true
+      }]
+    ]
   };
 };
--- src/apis/ipinfo.js
+++ src/apis/ipinfo.js
@@ -1,5 +1,10 @@
 import axios from "axios";
+import {IPINFO_APIKEY} from '@env';
 
 export default axios.create({
   baseURL: "https://ipinfo.io",
+  headers: {
+    Authorization:
+      `Bearer ${IPINFO_APIKEY}`,
+  },
 });
