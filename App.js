9f8297a2b85744c853c074e6f38f9986a78cddb8
AgungDH <agungdh@live.com>
2021-06-25 18:30:21 +0700

    add stack navigator for title

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import { Provider as PaperProvider, DefaultTheme } from 'react-native-paper';
+import { Provider as PaperProvider, DefaultTheme, BottomNavigation, Appbar } from 'react-native-paper';
 import IPInfoScreen from './src/screens/IPInfoScreen';
 import AboutScreen from './src/screens/AboutScreen';
-import { BottomNavigation } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-const App = () => {
+const HomeScreen = () => {
   const [index, setIndex] = React.useState(0);
   
   const [routes] = React.useState([
@@ -16,6 +18,16 @@ const App = () => {
     ipinfo: IPInfoScreen,
     about: AboutScreen,
   });
+
+  return (<BottomNavigation
+            navigationState={{ index, routes }}
+            onIndexChange={setIndex}
+            renderScene={renderScene}
+          />);
+}
+
+const App = () => {
+  const Stack = createStackNavigator();
 
   const theme = {
     ...DefaultTheme,
@@ -29,11 +41,11 @@ const App = () => {
 
   return (
     <PaperProvider theme={theme}>
-      <BottomNavigation
-        navigationState={{ index, routes }}
-        onIndexChange={setIndex}
-        renderScene={renderScene}
-      />
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen options={{title: "My IP Info", headerStyle: { backgroundColor: '#3498db' }}} name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </PaperProvider>
   );
 }
