054a83aabafc332aabdf3389f5a7142e857ad91d
AgungDH <agungdh@live.com>
2021-06-25 12:21:19 +0700

    add flag image

--- src/components/Flag.js
+++ src/components/Flag.js
@@ -1,1 +1,23 @@
 import React from 'react';
+import { View, Image, StyleSheet } from 'react-native';
+
+const Flag = ({countryCode}) => {
+    const imgSource = `https://www.countryflags.io/${countryCode}/flat/64.png`;
+
+    console.log({countryCode, imgSource});
+
+    return <View>
+        <Image style={styles.image} source={{uri: imgSource}} />
+    </View>
+}
+
+const styles = StyleSheet.create({
+    image: {
+        width: 64,
+        height: 64,
+        borderRadius: 4,
+        marginBottom: 5
+    },
+});
+
+export default Flag;
--- src/screens/IPInfoScreen.js
+++ src/screens/IPInfoScreen.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { SafeAreaView, Text } from 'react-native';
 import GlobalStyles from '../styles/GlobalStyles';
+import Flag from '../components/Flag';
+import ipinfo from '../apis/ipinfo';
+import { Title, Headline, Subheading  } from 'react-native-paper';
 
 const IPInfoScreen = () => {
+    const [ipInfo, setIpInfo] = useState(null);
+    const [countryCode, setCountryCode] = useState(null);
+
+    const getIpInfo = async () => {
+      const response = await ipinfo.get(`/`);
+
+      setIpInfo(response.data);
+      
+      setCountryCode(response.data.country);
+    };
+
+    useEffect(() => {
+        getIpInfo();
+    }, []);
+  
+    if (!ipInfo) {
+      return null;
+    }
+
     return <SafeAreaView style={GlobalStyles.droidSafeArea}>
-        <Text>Main</Text>
+        <Title>{ipInfo.ip}</Title>
+        <Subheading>{ipInfo.org}</Subheading>
+        <Flag countryCode={countryCode} />
     </SafeAreaView>
 };
 
