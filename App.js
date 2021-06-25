f53c893e937e9e2b4f9568abc2135756a9dccbfa
AgungDH <agungdh@live.com>
2021-06-25 17:45:47 +0700

    change theme

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Provider as PaperProvider } from 'react-native-paper';
+import { Provider as PaperProvider, DefaultTheme } from 'react-native-paper';
 import IPInfoScreen from './src/screens/IPInfoScreen';
 import AboutScreen from './src/screens/AboutScreen';
 import { BottomNavigation } from 'react-native-paper';
@@ -17,8 +17,18 @@ const App = () => {
     about: AboutScreen,
   });
 
+  const theme = {
+    ...DefaultTheme,
+    roundness: 2,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: '#3498db',
+      accent: '#f1c40f',
+    },
+  };
+
   return (
-    <PaperProvider>
+    <PaperProvider theme={theme}>
       <BottomNavigation
         navigationState={{ index, routes }}
         onIndexChange={setIndex}
